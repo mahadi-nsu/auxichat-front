d59c791283508685320960f2e392ff2535fd740a
SayedRaju1 <sayedraju21@gmail.com>
2022-03-22 11:57:14 +0600

    homepage done

--- pages/login.js
+++ pages/login.js
@@ -8,7 +8,13 @@ const login = () => {
     return (
         <div className="min-h-screen w-screen grid grid-cols-12">
 
-            <div className="col-span-6 bg-[#A4C4B5]"></div>
+            <div className="col-span-6 bg-[#A4C4B5] flex justify-center items-center">
+                <img
+                    className="w-2/3"
+                    src="./images/undraw_personal_opinions_re_qw29.svg"
+                    alt="banner image"
+                />
+            </div>
 
             <div className="col-span-6 flex justify-around items-center flex-col gap-9">
 
@@ -21,7 +27,7 @@ const login = () => {
                     <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
                         <label className="text-sm text-gray-400 font-semibold" htmlFor="email">Email</label>
                         <input
-                            className="border-b-2 py-1 text-lg text-gray-800 focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
+                            className="border-b-2 py-1 text-lg text-gray-800 focus:border-[#A4C4B5] focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
                             id="email"
                             type="email"
                             placeholder="enter your email here"
@@ -29,13 +35,13 @@ const login = () => {
                         />
                         <label className="text-sm text-gray-400 font-semibold mt-5" htmlFor="password">Password</label>
                         <input
-                            className="border-b-2 py-1 text-lg text-gray-800 focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
+                            className="border-b-2 py-1 text-lg text-gray-800 focus:border-[#A4C4B5] focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
                             id="password"
                             type="password"
                             placeholder="enter your password here"
                             {...register("Password", { required: true, maxLength: 100 })}
                         />
-                        <p className="text-[.60rem] text-blue-500 self-end pt-2 cursor-pointer">Forgot password ?</p>
+                        <p className="text-[.60rem] text-[#A4C4B5] self-end pt-2 cursor-pointer">Forgot password ?</p>
                         <input
                             className="my-5 bg-[#696969] text-[#fff] px-10 py-2 rounded-full cursor-pointer max-w-max self-center"
                             type="submit"
@@ -47,7 +53,7 @@ const login = () => {
                 <div>
                     <p className="text-sm text-gray-400">
                         New Here ?&nbsp;
-                        <span className="cursor-pointer text-blue-500">Create Account</span>
+                        <span className="cursor-pointer text-[#A4C4B5]">Create Account</span>
                     </p>
                 </div>
 
--- pages/register.js
+++ pages/register.js
@@ -8,7 +8,13 @@ const register = () => {
     return (
         <div className="min-h-screen w-screen grid grid-cols-12">
 
-            <div className="col-span-6 bg-[#A4C4B5]"></div>
+            <div className="col-span-6 bg-[#A4C4B5] flex justify-center items-center">
+                <img
+                    className="w-2/3"
+                    src="./images/undraw_podcast_re_wr88.svg"
+                    alt="banner image"
+                />
+            </div>
 
             <div className="col-span-6 flex justify-around items-center flex-col gap-9">
 
@@ -21,7 +27,7 @@ const register = () => {
                     <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
                         <label className="text-sm text-gray-400 font-semibold" htmlFor="name">Name</label>
                         <input
-                            className="border-b-2 py-1 text-lg text-gray-800 focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
+                            className="border-b-2 py-1 text-lg text-gray-800 focus:border-[#A4C4B5] focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
                             id="name"
                             type="text"
                             placeholder="enter your name here"
@@ -29,7 +35,7 @@ const register = () => {
                         />
                         <label className="text-sm text-gray-400 font-semibold mt-5" htmlFor="email">Email</label>
                         <input
-                            className="border-b-2 py-1 text-lg text-gray-800 focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
+                            className="border-b-2 py-1 text-lg text-gray-800 focus:border-[#A4C4B5] focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
                             id="email"
                             type="email"
                             placeholder="enter your email here"
@@ -37,7 +43,7 @@ const register = () => {
                         />
                         <label className="text-sm text-gray-400 font-semibold mt-5" htmlFor="password">Password</label>
                         <input
-                            className="border-b-2 py-1 text-lg text-gray-800 focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
+                            className="border-b-2 py-1 text-lg text-gray-800 focus:border-[#A4C4B5] focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
                             id="password"
                             type="password"
                             placeholder="enter new password here"
@@ -45,13 +51,13 @@ const register = () => {
                         />
                         <label className="text-sm text-gray-400 font-semibold mt-5" htmlFor="confirm password">Confirm Password</label>
                         <input
-                            className="border-b-2 py-1 text-lg text-gray-800 focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
+                            className="border-b-2 py-1 text-lg text-gray-800 focus:border-[#A4C4B5] focus:outline-none placeholder:italic placeholder:text-slate-300 placeholder:text-base"
                             id="confirm password"
                             type="password"
                             placeholder="re-enter new password here"
                             {...register("Password", { required: true, maxLength: 100 })}
                         />
-                        <p className="text-[.60rem] text-blue-500 self-end pt-2 cursor-pointer">Forgot password ?</p>
+                        <p className="text-[.70rem] text-[#A4C4B5] self-end pt-2 cursor-pointer">Forgot password ?</p>
                         <input
                             className="my-5 bg-[#696969] text-[#fff] px-10 py-2 rounded-full cursor-pointer max-w-max self-center"
                             type="submit"
@@ -63,7 +69,7 @@ const register = () => {
                 <div>
                     <p className="text-sm text-gray-400">
                         Already have an account ?&nbsp;
-                        <span className="cursor-pointer text-blue-500">Sign in here</span>
+                        <span className="cursor-pointer text-[#A4C4B5]">Sign in here</span>
                     </p>
                 </div>
 
