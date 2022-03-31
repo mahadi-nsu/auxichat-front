0db37d65d7ef91185a6fe462a89767a658668a8b
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-03-31 16:12:00 +0600

    Media Section added in the layout

--- components/ContactMedia/ContactMedia.js
+++ components/ContactMedia/ContactMedia.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactMedia = () => {
+  const images = [
+    "/images/image-1.png",
+    "/images/image-2.png",
+    "/images/image-3.png",
+    "/images/image-4.png",
+    "/images/image-5.png",
+    "/images/image-6.png",
+    "/images/image-7.png",
+    "/images/image-5.png",
+    "/images/image-6.png",
+    "/images/image-7.png",
+  ];
+  const [loadMore, setLoadMore] = useState(false);
+  const loadMoreHandle = () => {
+    setLoadMore(true);
+  };
+
+  console.log(loadMore);
   return (
-    <div className=" mt-2 mr-2 flex flex-col items-center">
+    <div className="mt-2 mr-2 flex flex-col items-center">
       <span className="text-[#6588DE] text-[18px] font-semibold">Media</span>
+      <div className="m-2 grid grid-rows-3 gap-5	grid-cols-3">
+        {images.map(
+          (image, i) =>
+            i <= 7 && (
+              <img
+                src={image}
+                key={i}
+                alt="image"
+                className="col-span-1 row-span-1"
+              />
+            )
+        )}
+
+        {!loadMore ? (
+          <div className="flex justify-center items-center">
+            <span className="cursor-pointer underline" onClick={loadMoreHandle}>
+              See more
+            </span>
+          </div>
+        ) : (
+          <>
+            {images.map(
+              (image, i) =>
+                i > 7 && (
+                  <img
+                    src={image}
+                    key={i}
+                    alt="image"
+                    className="col-span-1 row-span-1"
+                  />
+                )
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
